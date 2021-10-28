a5668127414551a1e7c8d86403797ee9cf49227b
meruzh <eremyanmerushan97@mail.ru>
2021-10-28 22:35:57 +0300

    lesson 37 done

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,61 +1,63 @@
-let rerenderEntireTree = () => {
-  console.log('changed')
-}
-
-let state = {
-  profilePage: {
-    posts: [
-      { id: 0, message: "Hey, why nobody love me?" },
-      { id: 1, message: "It's our new program! Hey!" },
-    ],
-    newPostText: "",
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 0, message: "Hey, why nobody love me?" },
+        { id: 1, message: "It's our new program! Hey!" },
+      ],
+      newPostText: "",
+    },
+    dialogsPage: {
+      dialogs: [
+        { id: 1, name: "Viktor" },
+        { id: 2, name: "Dmitry" },
+        { id: 3, name: "Sasha" },
+        { id: 4, name: "Sveta" },
+        { id: 5, name: "Valera" },
+        { id: 6, name: "Andrew" },
+        { id: 7, name: "Victoria" },
+        { id: 8, name: "Ruslan" },
+        { id: 9, name: "Elena" },
+        { id: 10, name: "Antonio" },
+      ],
+  
+      messages: [
+        { id: 1, message: "Hi" },
+        { id: 2, message: "How are you?" },
+        { id: 3, message: "What's app man" },
+      ],
+    },
+    navbar: {
+      friends: [
+        { id: 1, name: "Viktor" },
+        { id: 2, name: "Dmitry" },
+        { id: 3, name: "Sasha" },
+        { id: 4, name: "Sveta" },
+        { id: 5, name: "Valera" },
+        { id: 6, name: "Andrew" },
+      ],
+    },
   },
-  dialogsPage: {
-    dialogs: [
-      { id: 1, name: "Viktor" },
-      { id: 2, name: "Dmitry" },
-      { id: 3, name: "Sasha" },
-      { id: 4, name: "Sveta" },
-      { id: 5, name: "Valera" },
-      { id: 6, name: "Andrew" },
-      { id: 7, name: "Victoria" },
-      { id: 8, name: "Ruslan" },
-      { id: 9, name: "Elena" },
-      { id: 10, name: "Antonio" },
-    ],
-
-    messages: [
-      { id: 1, message: "Hi" },
-      { id: 2, message: "How are you?" },
-      { id: 3, message: "What's app man" },
-    ],
+  _callSubscriber(){
+    console.log('changed')
   },
-  navbar: {
-    friends: [
-      { id: 1, name: "Viktor" },
-      { id: 2, name: "Dmitry" },
-      { id: 3, name: "Sasha" },
-      { id: 4, name: "Sveta" },
-      { id: 5, name: "Valera" },
-      { id: 6, name: "Andrew" },
-    ],
+  addPost(){
+    this._state.profilePage.posts.push({ id: 2, message: this._state.profilePage.newPostText });
+    this._state.profilePage.newPostText = ""
+    this._callSubscriber()
   },
+  updateNewPostText(newText){
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber();
+  },
+  subscribe(observer){
+    this._callSubscriber = observer;
+  },
+  getState(){
+    return this._state;
+  }
 };
 
-// Window.prototype.state = state;
-export let addPost = () => {
-  state.profilePage.posts.push({ id: 2, message: state.profilePage.newPostText });
-  state.profilePage.newPostText = ""
-  rerenderEntireTree()
-};
+Window.prototype.store = store;
 
-export let updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree();
-};
-
-export let subscribe = (observer) => {
-  rerenderEntireTree = observer;
-}
-
-export default state;
+export default store;
