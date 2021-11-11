a904d761f82ebeaed49a91ea48977a07cf361fad
meruzh <eremyanmerushan97@mail.ru>
2021-11-11 15:00:58 +0300

    lesson 54 done

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,11 +4,10 @@ import userPhoto from '../../assets/images/1077114.png'
 
 class Users extends React.Component {
 
-    constructor(props){
-        super(props);
+    componentDidMount(){
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(r => {
                 this.props.setUsers(r.data.items)
-            })
+        })
     }
 
     render(){
